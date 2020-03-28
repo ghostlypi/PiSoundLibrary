8e20d5cb4cdbd21faeca40438fff4d4e8e0e6547
ghostlypi <parth.iyer@gmail.com>
2020-03-28 12:07:36 -0700

    Update Libgen.py

--- Libgen/Libgen.py
+++ Libgen/Libgen.py
@@ -3,9 +3,8 @@ import os
 import time
 
 start = time.time()
-for i in range(20,20):
-    for j in range(100,101):
-        SoundBoard.build([[0,9,0.125,j,i]])
-        SoundBoard.compile("LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav")
-        #os.replace("/Users/parth/Desktop/Music/Libgen/LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav","/Users/parth/Desktop/Music/Libgen/FREQ="+str(i)+"/LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav")
+for i in range(20,20000):
+    for j in range(1,101):
+        SoundBoard.compile([[0,9,0.125,j,i]],"LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav")
+        os.replace("/Users/parth/Desktop/Music/Libgen/LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav","/Users/parth/Desktop/Music/Libgen/FREQ="+str(i)+"/LibgenFREQ="+str(i)+"&AMP="+str(j)+".wav")
     print("FREQ="+str(i)+" Completed! "+str(time.time()-start))
